ba3411a1c1fb39e9d305c24b02bbf678568456a6
omkamble <omk10902@gmail.com>
2023-04-12 10:19:18 +0530

    Added Firebase to the project. Login and register page made function using firebase auth

--- Partyy-fyy/loginViewController.swift
+++ Partyy-fyy/loginViewController.swift
@@ -6,15 +6,29 @@
 //
 
 import UIKit
-
+import FirebaseAuth
 class loginViewController: UIViewController {
-
+    @IBOutlet weak var emailID: UITextField!
+    
+    @IBOutlet weak var passwordf: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
     }
     
-
+    @IBAction func LoginButton(_ sender: UIButton) {
+        if let email = emailID.text, let password = passwordf.text{
+            Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
+              guard let strongSelf = self else { return }
+                if let e = error{
+                    print(e)
+                }else{
+                    self.performSegue(withIdentifier: "loginPass", sender: self)
+                }
+            }
+        }
+    }
+    
    
 }
--- Partyy-fyy/registerViewController.swift
+++ Partyy-fyy/registerViewController.swift
@@ -6,15 +6,28 @@
 //
 
 import UIKit
-
+import FirebaseAuth
 class registerViewController: UIViewController {
 
+    @IBOutlet weak var passwordF: UITextField!
+    @IBOutlet weak var emailiD: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     
-
+    @IBAction func registerButton(_ sender: UIButton) {
+        if let email = emailiD.text, let password = passwordF.text{
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error{
+                    print(e)
+                }else{
+                    self.performSegue(withIdentifier: "registerPass", sender: self)
+                }
+            }
+        }
+    }
+    
     
 
 }
