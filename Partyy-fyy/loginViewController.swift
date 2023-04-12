a8d9a723891ed801858e7718d07cfb11b22a92da
omkamble <omk10902@gmail.com>
2023-04-12 10:04:14 +0530

    Initial setup for Partifyy app

---
+++ Partyy-fyy/loginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  loginViewController.swift
+//  Partyy-fyy
+//
+//  Created by OM KAMBLE on 12/04/23.
+//
+
+import UIKit
+
+class loginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+   
+}
---
+++ Partyy-fyy/registerViewController.swift
@@ -0,0 +1,20 @@
+//
+//  registerViewController.swift
+//  Partyy-fyy
+//
+//  Created by OM KAMBLE on 12/04/23.
+//
+
+import UIKit
+
+class registerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    
+
+}
